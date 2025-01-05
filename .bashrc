8b4ac42df649fa34a640560407ab54dd510f949f
Darren Chin <d.chin1@icloud.com>
2025-01-05 12:45:02 -0500

    Updates

--- .bashrc
+++ .bashrc
@@ -7,7 +7,8 @@
 
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-PS1='[\u@\h \W]\$ '
+#PS1='[\u@\h \W]\$ '
+PS1='\[\e[32m\]\u@\h\[\e[0m\] \[\e[34m\]\W\[\e[0m\] \$ '
 
 EDITOR=vim
 PATH=$PATH:~/bin
--- .config/nvim/lua/darrenchin/plugins/lsp/mason.lua
+++ .config/nvim/lua/darrenchin/plugins/lsp/mason.lua
@@ -27,7 +27,7 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "ansiblels@v1.2.0",
+        "ansiblels",
         "bashls",
         "gopls",
         "helm_ls",
